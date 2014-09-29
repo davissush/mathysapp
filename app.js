a1f697ce938fcb8b0ca7273bf76c21895331f74d
davissush <davis@sush.co.nz>
2014-09-30 10:58:01 +1300

    Update app.js

--- app.js
+++ app.js
@@ -49,10 +49,10 @@ function route(event) {
         var remotehost = 'http://mathyscms.edith.techrus.co.nz/'
 
         if(!searchpage.trim()){
-                slider.slidePage($("<a href='#page-2'>goto next</a>"));
+                renderhomepage();
         } else {
                 pageid=searchpage.substring(5);
-                slider.slidePage($("<a href='#'>" + pageid + "</a>"));
+                rendercategorypage(pageid);
         }
 }
 
