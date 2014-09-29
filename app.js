e4ca848d7627b7154fb27c23a1e88e2ba2086f6e
davis <davis.dimalen@gmail.com>
2014-09-30 10:02:26 +1300

    Update for testing

--- app.js
+++ app.js
@@ -1,98 +1,51 @@
 window.addEventListener('load', function () {
-    new FastClick(document.body);
+        new FastClick(document.body);
 }, false);
-
 
 var slider = new PageSlider($("#container"));
 $(window).on('hashchange', route);
 
-var slider = new PageSlider($("#container"));
-var spinner = $("#spinner");
-
-spinner.hide();
-
 $('.goback').entwine({
         onclick: function(e){
-	e.preventDefault();
-	parent.history.back();
-     }
+                e.preventDefault();
+                parent.history.back();
+        }
 });
 
-function showMessage(message, title) {
-        if (navigator.notification) {
-                navigator.notification.alert(message, null, title, 'OK');
+// Basic page routing
+function route(event) {
+        var hash = window.location.hash;
+
+        var searchpage = hash.substring(1);
+        var remotehost = 'http://mathyscms.edith.techrus.co.nz/'
+
+
+        if(!searchpage.trim()){
+                $.ajax({
+                        type: 'GET',
+                        url: remotehost + '/mathys_api/get-main-categories',
+                        jsonp: "callback",
+                        dataType: "jsonp",
+                        complete: function(){
+                        },
+                        success: function(data){
+                                slider.slidePage($(data));
+                        }
+                });
         } else {
-                alert(title ? (title + ": " + message) : message);
+                $pageid=searchpage.substring(5);
+                $.ajax({
+                        type: 'GET',
+                        url: remotehost + '/mathys_api/get_subcategory_page/?pageid='+$pageid,
+                        jsonp: "callback",
+                        dataType: "jsonp",
+                        complete: function(){
+                        },
+                        success: function(data){
+                                slider.slidePage($(data));
+                        }
+                });
         }
 }
 
-
-function route(event) {
-var page,
-    hash = window.location.hash;
-
-                        var searchpage = hash.substring(1);
-
-                        //var remotehost = 'http://mathyscms.edith.techrus.co.nz'
-                        var remotehost = 'http://mathyscms.edith.techrus.co.nz/'
-
-                        $('.debug').html(hash);
-
-                        showMessage(hash,'Top');
-
-                        if(!searchpage.trim()){
-                                spinner.hide();
-                                spinner.show();
-                                $.ajax({
-                                        type: 'GET',
-                                        url: remotehost + '/mathys_api/get-main-categories',
-                                        jsonp: "callback",
-                                        dataType: "jsonp",
-                                        complete: function(){
-                                        },
-                                        success: function(data){
-                                                spinner.hide();
-                                                slider.slidePage($(data));
-                                        }
-                                });
-                        } else {
-                                window.alert(hash);
-                                if(searchpage.substring(0,4) == 'page'){
-                                        $pageid=searchpage.substring(5);
-                                        spinner.hide();
-                                        spinner.show();
-                                        $.ajax({
-                                                type: 'GET',
-                                                url: remotehost + '/mathys_api/get_subcategory_page/?pageid='+$pageid,
-                                                jsonp: "callback",
-                                                dataType: "jsonp",
-                                                complete: function(){
-                                                },
-                                                success: function(data){
-                                                        spinner.hide();
-                                                        slider.slidePage($(data));
-                                                }
-                                        });
-                                } else {
-                                        $pageid=searchpage.substring(5);
-                                        spinner.hide();
-                                        spinner.show();
-                                        $.ajax({
-                                                type: 'GET',
-                                                url: remotehost + '/mathys_api/get_media_page/?pageid='+$pageid,
-                                                jsonp: "callback",
-                                                dataType: "jsonp",
-                                                complete: function(){
-                                                },
-                                                success: function(data){
-                                                        spinner.hide();
-                                                        slider.slidePage($(data));
-                                                }
-                                        });
-                                }
-                        }
-
-}
-
 route();
-
