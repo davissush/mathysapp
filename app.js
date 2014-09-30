a4af1726a22efc77472dea851a7301e377d9d84e
davis <davis.dimalen@gmail.com>
2014-10-01 10:44:41 +1300

    Comment out localcal storage remove isregistered to bypass form

--- app.js
+++ app.js
@@ -90,7 +90,7 @@ function route(event) {
         spinner.show();
         if(!searchpage.trim()){
 
-                window.localStorage.removeItem('isregistered');
+                //window.localStorage.removeItem('isregistered');
 
                 //console.log(window.localStorage.getItem("isregistered"));
 
